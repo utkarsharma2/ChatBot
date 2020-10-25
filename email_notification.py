91b663984457afac1403eb3c7db5f54083eda8ab
Srivishnu Ayyagari <ayyagari@dalalstreet.ai>
2020-10-25 20:42:22 +0530

    Adds Mailgun config

--- email_notification.py
+++ email_notification.py
@@ -1,11 +1,11 @@
 import smtplib
 
-gmail_user = '@gmail.com'
-gmail_password = ''
+gmail_user = 'postmaster@sandbox418cf31285a1465fb3c25cff4ba476b2.mailgun.org'
+gmail_password = '2b1d910d1a368f92941595fddc176e16-53c13666-c38d6ff0'
 
 sent_from = gmail_user
-to = ['me@gmail.com', 'bill@gmail.com']
-subject = 'OMG Super Important Message'
+to = 'vishnu.ayyagari@gmail.com'
+subject = 'This is a test mail'
 body = 'Hey, whats up?\n\n- You'
 
 email_text = """\
@@ -17,10 +17,10 @@ Subject: %s
 """ % (sent_from, ", ".join(to), subject, body)
 
 
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 587)
-    server.ehlo()
-    server.login(gmail_user, gmail_password)
-    server.sendmail(sent_from, to, email_text)
-    server.close()
+server = smtplib.SMTP_SSL('smtp.mailgun.org', 465)
+server.ehlo()
+server.login(gmail_user, gmail_password)
+server.sendmail(sent_from, to, email_text)
+server.close()
 
-    print ('Email sent!')
+print ('Email sent!')
